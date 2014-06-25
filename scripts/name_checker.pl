cf213157f2e7b7dc8b478ead2aa3a399e87a593f
Jason Stajich <jason@bioperl.org>
2014-06-25 14:59:31 -0700

    name fixer for short reads

---
+++ scripts/name_checker.pl
@@ -0,0 +1,36 @@
+#!env perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+=head1 NAME 
+
+fasta_name_checker - fix names which a
+
+=head1 USAGE
+
+ -t / --title  specify the read title base (default: Unknown)
+ -p / --prefix specify the read prefix (default: UNK)
+
+=head1 AUTHOR Jason Stajich
+
+Jason Stajich - jason[at]bioperl.org
+
+=cut
+
+my $count = 1;
+my $title = 'Unknown';
+my $prefix = 'UNK';
+
+GetOptions("t|title:s"  => \$title,
+	   "p|prefix:s" => \$prefix);
+while(<>) {
+    if( /^>/ ) {
+# $count keeps track of number of unique IDs for replacement
+	if( s/no name/$title\_$count/i ) { $count++ }
+	if( ! /\|/ ) {
+	    s/>/>$prefix|/;
+	}
+    }
+    print;
+}
